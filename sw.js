c2307029fd8197c71150c7180e4edf86bad48afb
woeitarng-coder <woeitarng@gmail.com>
2026-03-25 00:51:53 +0800

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 獨立快取前綴，確保不與其他 App 衝突
 const CACHE_PREFIX = 'golfpro-cache-';
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 
 const urlsToCache = [
